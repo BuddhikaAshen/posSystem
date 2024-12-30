f6260d93e1ffac113a2982b57dc78432d31cfc5b
DinendraRathnayake <dsrathnayake4@gmail.com>
2024-12-30 20:36:24 +0530

    add button

--- posSystem/dashboard.Designer.cs
+++ posSystem/dashboard.Designer.cs
@@ -36,13 +36,15 @@
             this.stock = new System.Windows.Forms.Button();
             this.profit = new System.Windows.Forms.Button();
             this.sale = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // check
             // 
-            this.check.Location = new System.Drawing.Point(103, 122);
+            this.check.Location = new System.Drawing.Point(116, 152);
+            this.check.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.check.Name = "check";
-            this.check.Size = new System.Drawing.Size(75, 40);
+            this.check.Size = new System.Drawing.Size(84, 50);
             this.check.TabIndex = 0;
             this.check.Text = "Check";
             this.check.UseVisualStyleBackColor = true;
@@ -51,9 +53,9 @@
             // lblhide
             // 
             this.lblhide.AutoSize = true;
-            this.lblhide.Location = new System.Drawing.Point(224, 20);
+            this.lblhide.Location = new System.Drawing.Point(252, 25);
             this.lblhide.Name = "lblhide";
-            this.lblhide.Size = new System.Drawing.Size(42, 16);
+            this.lblhide.Size = new System.Drawing.Size(50, 20);
             this.lblhide.TabIndex = 1;
             this.lblhide.Text = "Role: ";
             this.lblhide.Click += new System.EventHandler(this.label1_Click);
@@ -61,16 +63,17 @@
             // lblout
             // 
             this.lblout.AutoSize = true;
-            this.lblout.Location = new System.Drawing.Point(301, 20);
+            this.lblout.Location = new System.Drawing.Point(339, 25);
             this.lblout.Name = "lblout";
-            this.lblout.Size = new System.Drawing.Size(0, 16);
+            this.lblout.Size = new System.Drawing.Size(0, 20);
             this.lblout.TabIndex = 2;
             // 
             // reg
             // 
-            this.reg.Location = new System.Drawing.Point(103, 168);
+            this.reg.Location = new System.Drawing.Point(116, 210);
+            this.reg.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.reg.Name = "reg";
-            this.reg.Size = new System.Drawing.Size(75, 40);
+            this.reg.Size = new System.Drawing.Size(84, 50);
             this.reg.TabIndex = 0;
             this.reg.Text = "Register";
             this.reg.UseVisualStyleBackColor = true;
@@ -78,9 +81,10 @@
             // 
             // item
             // 
-            this.item.Location = new System.Drawing.Point(103, 214);
+            this.item.Location = new System.Drawing.Point(116, 268);
+            this.item.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.item.Name = "item";
-            this.item.Size = new System.Drawing.Size(75, 40);
+            this.item.Size = new System.Drawing.Size(84, 50);
             this.item.TabIndex = 0;
             this.item.Text = "Add Items";
             this.item.UseVisualStyleBackColor = true;
@@ -88,9 +92,10 @@
             // 
             // stock
             // 
-            this.stock.Location = new System.Drawing.Point(103, 260);
+            this.stock.Location = new System.Drawing.Point(116, 325);
+            this.stock.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.stock.Name = "stock";
-            this.stock.Size = new System.Drawing.Size(75, 40);
+            this.stock.Size = new System.Drawing.Size(84, 50);
             this.stock.TabIndex = 0;
             this.stock.Text = "Stock";
             this.stock.UseVisualStyleBackColor = true;
@@ -98,9 +103,10 @@
             // 
             // profit
             // 
-            this.profit.Location = new System.Drawing.Point(103, 348);
+            this.profit.Location = new System.Drawing.Point(116, 435);
+            this.profit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.profit.Name = "profit";
-            this.profit.Size = new System.Drawing.Size(75, 40);
+            this.profit.Size = new System.Drawing.Size(84, 50);
             this.profit.TabIndex = 0;
             this.profit.Text = "Report";
             this.profit.UseVisualStyleBackColor = true;
@@ -108,19 +114,30 @@
             // 
             // sale
             // 
-            this.sale.Location = new System.Drawing.Point(103, 306);
+            this.sale.Location = new System.Drawing.Point(116, 382);
+            this.sale.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.sale.Name = "sale";
-            this.sale.Size = new System.Drawing.Size(75, 40);
+            this.sale.Size = new System.Drawing.Size(84, 50);
             this.sale.TabIndex = 0;
             this.sale.Text = "Sale";
             this.sale.UseVisualStyleBackColor = true;
             this.sale.Click += new System.EventHandler(this.sale_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(725, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblout);
             this.Controls.Add(this.lblhide);
             this.Controls.Add(this.sale);
@@ -129,6 +146,7 @@
             this.Controls.Add(this.item);
             this.Controls.Add(this.reg);
             this.Controls.Add(this.check);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "dashboard";
             this.Text = "dashboard";
             this.ResumeLayout(false);
@@ -146,5 +164,6 @@
         private System.Windows.Forms.Button stock;
         private System.Windows.Forms.Button profit;
         private System.Windows.Forms.Button sale;
+        private System.Windows.Forms.Button button1;
     }
 }
